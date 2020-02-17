e5edb7639470bfe6fc16e7317d081cfced164929
maskyss <maskyss932@gmail.com>
2020-02-17 21:19:22 +0200

    add config

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,50 @@
 import React, { useEffect } from "react";
-import Link from "next/link";
-import { withContentful } from "next-contentful";
-import App, { Container } from 'next/app';
-// Contentful config properties
-const space = "q3zcl005fqwg";
-const accessToken = "RPrUdMfYY0V4ZO6IfUYTBxPQPHxkr3UYoEypDCRshUI";
-const host = "cdn.contentful.com";
-const locale = "es-US";
-class MyApp extends App {
-    static async getInitialProps({ Component, ctx, router }) {
-      let pageProps = {};
+import Head from "next/head";
+import { createClient } from "contentful";
+import Post from "../components/post";
+import config from "./config.json";
+
+// Instantiate the app client
+const client = createClient({
+  space: config.space,
+  accessToken: config.accessToken
+});
+
+// Our Homepage component, will receive props from contentful entries thanks to getInitialProps function below.
+function HomePage(props) {
+  useEffect(()=>{
+    console.log(props.entries.items)
+
+  },[])
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Welcome to NextJS + Contentful by ScreamZ</title>
+        <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre.min.css" />
+        <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-exp.min.css" />
+        <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-icons.min.css" />
+      </Head>
+      <div className="container grid-lg mt-2">
+        <div className="columns">
+          {/* {props.allPosts && props.allPosts.map(post => <Post post={post} key={post.fields.title} />)} */}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+// This function will run during build time in case of static export.
+// Or will run each time a new request is made to the browser in SSR.
+// It's used to compute initial props for the component and pre-render.
+HomePage.getInitialProps = async () => {
+  const entries = await client.getEntries({
+    content_type: "blogPost",
   
-      if (Component.getInitialProps) {
-        pageProps = await Component.getInitialProps({ ctx });
-      }
-  
-      return { pageProps };
-    }
-  
-    render() {
-      const {
-        Component,
-        pageProps,
-        store,
-      } = this.props;
-  
-      return (
-        <Container>
-          <Component {...pageProps} />
-        </Container>
-      );
-    }
-  }
-  
-  export default withContentful(
-    space,
-    accessToken,
-    host,        
-    locale,       
-  )(MyApp);
+  });
+
+  // Inject in props of our screen component
+  return {entries };
+};
+
+// That's the default export (the page)
+export default HomePage;
